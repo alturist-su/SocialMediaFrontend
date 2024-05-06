df996933f94bcdc5e122aa97a4d6cd757b08b668
alturist-su <aimsushmitasingh@gmail.com>
2024-05-07 00:32:00 +0530

    Sidebar Config completed

--- src/Components/Sidebar/SidebarConfig.js
+++ src/Components/Sidebar/SidebarConfig.js
@@ -1,5 +1,30 @@
-import { AiOutlineHome, AiFillHome } from "react-icons/ai"
+import { AiOutlineHome, AiFillHome, AiOutlineSearch, AiOutlineCompass, AiFillCompass, AiOutlineMessage, AiFillMessage, AiOutlineNotification, AiFillNotification, AiOutlinePlusCircle, AiFillPlusCircle } from "react-icons/ai"
+import { CgProfile } from "react-icons/cg"
+import { RiVideoFill, RiVideoLine } from "react-icons/ri"
 
 const menu = [
-    {title:"Home", icon: <AiOutlineHome></AiOutlineHome>, iactiveIcon: <AiFillHome></AiFillHome>}
+    {
+        title:"Home", icon: <AiOutlineHome></AiOutlineHome>, activeIcon: <AiFillHome></AiFillHome>
+    },
+    {
+        title:"Search", icon: <AiOutlineSearch></AiOutlineSearch>, activeIcon: <AiOutlineSearch></AiOutlineSearch>
+    },
+    {
+        title:"Explore", icon: <AiOutlineCompass></AiOutlineCompass>, activeIcon: <AiFillCompass></AiFillCompass>
+    },
+    {
+        title:"Reels", icon: <RiVideoLine></RiVideoLine>, activeIcon: <RiVideoFill></RiVideoFill>
+    },
+    {
+        title:"Message", icon: <AiOutlineMessage></AiOutlineMessage>, activeIcon: <AiFillMessage></AiFillMessage>
+    },
+    {
+        title:"Notification", icon: <AiOutlineNotification></AiOutlineNotification>, activeIcon: <AiFillNotification></AiFillNotification>
+    },
+    {
+        title:"Create", icon: <AiOutlinePlusCircle></AiOutlinePlusCircle>, activeIcon: <AiFillPlusCircle></AiFillPlusCircle>
+    },
+    {
+        title:"Profile", icon: <CgProfile></CgProfile>, activeIcon: <CgProfile></CgProfile>
+    }
 ]
