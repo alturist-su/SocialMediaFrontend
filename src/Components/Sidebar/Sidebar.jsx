1a5713df3ddb4d8208e95cf514c526ef84be8692
alturist-su <aimsushmitasingh@gmail.com>
2024-05-11 01:34:35 +0530

    Added onclick on icons

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { IoReorderThree } from "react-icons/io5";
 import { menu } from './SidebarConfig';
+import { useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
+    const [activeTab, setActiveTab] = useState();
+    const navigate = useNavigate();
+    
+  const handleTabClick=(title)=>{
+    setActiveTab(title);
+    if(title === 'Profile'){
+        navigate("/username");
+    }
+    else if(title === "Home"){
+        navigate("/")
+    }
+
+  }
+
   return (
     <div className='sticky top-0 h-[100vh]'>
         <div className='flex flex-col justify-between h-full px-10'>
@@ -12,8 +27,8 @@ const Sidebar = () => {
             </div>
             <div className='mt-10'>
                 {menu.map((item)=>
-                    <div className='flex items-center mb-5 cursor-pointer text-lg'> 
-                    {item.icon}
+                    <div onClick={() => handleTabClick(item.title)} className='flex items-center mb-5 cursor-pointer text-lg'> 
+                    {activeTab === item.title ? item.activeIcon : item.icon}
                     <p>{item.title}</p>
                 </div> 
                 )}
