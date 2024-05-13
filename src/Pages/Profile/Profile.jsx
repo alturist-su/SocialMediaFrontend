088632195f02072b79e893794db2fbfebd06adc5
alturist-su <aimsushmitasingh@gmail.com>
2024-05-14 00:40:15 +0530

    ReqUserPostPart component added

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ProfileUserDetails from '../../Components/ProfileComponents/ProfileUserDetails'
+import ReqUserPostPart from '../../Components/ProfileComponents/ReqUserPostPart'
 
 const Profile = () => {
   return (
-    <div>
-        <div className='px-20'>
+    <div className='px-20'>
+        <div>
             <ProfileUserDetails/>
         </div>
+        <div>
+            <ReqUserPostPart/>
+        </div>
+
     </div>
   )
 }
