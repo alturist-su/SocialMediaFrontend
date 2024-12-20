a184efc159df95597fbe4669ef59d519af2b5de2
susingh <aimsushmitasingh@gmail.com>
2024-12-20 19:33:31 +0530

    Update Router to navigate to Profile page by default

--- src/Pages/Router/Router.jsx
+++ src/Pages/Router/Router.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Sidebar from '../../Components/Sidebar/Sidebar'
-import HomePage from '../HomePage/HomePage'
-import Profile from '../Profile/Profile'
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import HomePage from '../HomePage/HomePage';
+import Profile from '../Profile/Profile';
 
 const Router = () => {
   return (
     <div>
-        <div className='flex'>
-            <div className='width-[20%] border border-l-slate-500'>
-                <Sidebar/>
-            </div>
-            <div className='w-full'>
-                <Routes>
-                    <Route path="/" element={<HomePage/>}></Route>
-                    <Route path='/username' element={<Profile/>}></Route>
-                </Routes> 
-            </div>
+      <div className='flex'>
+        <div className='width-[20%] border border-l-slate-500'>
+          <Sidebar />
         </div>
+        <div className='w-full'>
+          <Routes>
+            <Route path="/" element={<Navigate to="/username" />} />
+            <Route path="/username" element={<Profile />} />
+          </Routes>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
